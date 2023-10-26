6bfb49f7343cd2a20894176aa259d0a81d8f060b
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-10-26 10:19:08 +0600

    Warnings removed

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,5 @@
+// ignore: unused_import
 import 'package:flutter/material.dart';
 
-const base_url = "http://api.weatherapi.com/v1/forecast.json?key=4c86cc5911e74b6abe9102831230203";
-
+const base_url =
+    "http://api.weatherapi.com/v1/forecast.json?key=4c86cc5911e74b6abe9102831230203";
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:flutter_weather_bg_null_safety/bg/weather_bg.dart';
 import 'package:flutter_weather_bg_null_safety/utils/weather_type.dart';
